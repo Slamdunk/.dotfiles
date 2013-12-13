d5216287d945223ca902741bc2abd6c6bbd73bea
Filippo Tessarotto <zoeslam@gmail.com>
2013-12-13 16:40:04 +0100

    Install.sh eseguibile

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ DOTFILES_ROOT="$(readlink -f $(dirname "$0"))"
 suffix=".""$(date +%y%m%d.%H%M)"".bck"
 
 if [ "$USER" == "root" ]; then
-    chmod 0755 "$DOTFILES_ROOT"
-    find "$DOTFILES_ROOT" -type f -maxdepth 1 -exec chmod 0644 {} \;
-
     for file in "profile:/etc/profile" "bashrc:/etc/bash.bashrc" "vimrc:/etc/vim/vimrc.local"; do
         from="$(echo -n "$file" | sed 's/:.*//g')"
         to="$(echo -n "$file" | sed 's/.*://g')"
