171d7f23567ff6c7aa1d2e41ea0de89e766ecf43
Filippo Tessarotto <zoeslam@gmail.com>
2019-03-12 16:06:04 +0100

    Ask for overwrite with diff

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-set -ex
+thisDir=$(readlink --canonicalize "$(dirname "$0")")
 
-thisDir=$(readlink --canonicalize $(dirname "$0"))
+for file in \
+    ".bashrc" \
+    ".gitconfig" \
+    ".tmux.conf" \
+    ".vimrc" \
+    ".wgetrc" \
+; do
+    destination=~/"$file"
+    newfile="${thisDir:?}""/""$file"
+    if [ -f "$destination" ]; then
+        diffOutput=$(diff "$destination" "$newfile")
+        [ $? = 1 ] || continue
+        echo "$diffOutput"
 
-rsync \
-    --exclude ".git/" \
-    --exclude "install.sh" \
-    --exclude "LICENSE" \
-    --exclude "README.md" \
-    -avh --no-perms "${thisDir:?}"/. ~
+		read -p "Update [""$file""] ? " -n 1 -r < /dev/tty
+		echo
+		[[ "$REPLY" =~ ^[Yy]$ ]] || continue
+    fi
+
+	cp -av "$newfile" "$destination"
+done
